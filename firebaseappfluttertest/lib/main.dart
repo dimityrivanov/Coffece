c9f703e75643d6c6c4a3727b97b452316d841671
Dimitar Ivanov <dimitarivanovit@gmail.com>
2019-11-30 15:35:30 +0200

    Changed app name

--- firebaseappfluttertest/lib/main.dart
+++ firebaseappfluttertest/lib/main.dart
@@ -17,7 +17,7 @@ class CoffeeList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Coffice',
       theme: ThemeData(
         // This is the theme of your application.
         //
